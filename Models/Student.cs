a93b4ed65ad59556c5c6fd5818a5740affb7a6e1
Claudia Ulloa Campabadal <102692545+claudiaulloacampabadal@users.noreply.github.com>
2025-02-23 18:19:15 -0600

    Update Student.cs

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,15 +1,36 @@
-﻿namespace ProyectoStudent.Models
+using System.Text.Json.Serialization;
+
+namespace ProyectoStudent.Models
 {
     public class Student
     {
         public int Id { get; set; }
+
+        [JsonPropertyName("name")]
         public string Name { get; set; }
+
+        [JsonPropertyName("lastName")]
+
         public string LastName { get; set; }
+
+        [JsonPropertyName("email")]
+
         public string Email { get; set; }
+
+        [JsonPropertyName("userName")]
+
         public string UserName { get; set; }
+
+        [JsonPropertyName("password")] 
         public string Password { get; set; }
         public byte?[] Photo { get; set; } // Para almacenar imágenes
+
+        [JsonPropertyName("SocialLinks")]
+
         public string? SocialLinks { get; set; }
+
+        [JsonPropertyName("statusStudent")]
+
         public bool? StatusStudent { get; set; } // true = activo, false = inactivo
     }
 }
